dd307c63e54195c304669403c323fb99f3173474
omararod99 <58446105+omararod99@users.noreply.github.com>
2024-12-05 00:00:15 -0600

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -28,49 +28,22 @@ def report_safe(x):
     return strict_monotonic(x) and adjacent(x)
 
 def damper(x):        
-    total_neg = 0
-    total_pos = 0
-    differences = []
-    # print(x)
-
-    # run patcher
-
-    # find if increment or decrement 
-    for l,m in zip(range(0,len(x)),range(1,len(x))):
-        if x[m]-x[l] < 0:
-            total_neg += 1
-        if x[m]-x[l] > 0:
-            total_pos += 1
+    for i in range(0,len(x)):
+        x_changed = x.copy()
+        x_changed.pop(i)
         
-        differences.append(x[m]-x[l])   
-    print("Differences: " + str(differences))
-
-    if total_neg > total_pos:
-        matches = [x for x in range(0,len(differences)) if differences[x] > -1 or differences[x] < -3]
-        # print(matches)
-        if matches[0]+1 == len(differences):
-            x.pop(matches[0]+1)
+        if report_safe(x_changed):
+            x = x_changed.copy()
+            break
         else:
-            x.pop(matches[0])
-
-    if total_pos > total_neg:
-        matches = [x for x in range(0,len(differences)) if differences[x] > 3 or differences[x] < 1]
-        print(matches)
-        if matches[0]+1 == len(differences):
-            x.pop(matches[0]+1)
-        elif differences[matches[0]] > 3 and differences[matches[0]+1] < 1:
-            x.pop(matches[1])
-        else:
-            x.pop(matches[0])
-
-    # print(x)
+            x_changed = x.copy()
 
     return report_safe(x)
 
     
-# add safe rows
+# count safe rows
 for n in rows_div:
-    print("Original report: " + str(n))
+    print("Report: " + str(n))
     if report_safe(n):
         safe_report_count.append(True)
         print("Safe")
@@ -80,9 +53,6 @@ for n in rows_div:
             print("Changes: " + str(n))
             print("Safe")
         else:
-            # print(n)
-            print("unsafe")
-    
-
+            print("Unsafe")
 
 print(sum(safe_report_count))
